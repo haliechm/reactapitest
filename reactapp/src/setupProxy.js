dd581ecb45809c43829b920a82030ff2d085a2cc
Halie Chmura <halie@sightsource.net>
2023-06-29 16:53:31 -0400

    first

---
+++ reactapp/src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const context = [
+    "/weatherforecast",
+];
+
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: 'https://localhost:7074',
+        secure: false
+    });
+
+    app.use(appProxy);
+};
